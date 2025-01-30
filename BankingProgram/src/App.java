0a993389505b0e69effbdd26ceb225206a3c1866
HKPD101 <hrithikgowda006@gmail.com>
2025-01-30 22:23:01 +0530

    cmv

--- BankingProgram/src/App.java
+++ BankingProgram/src/App.java
@@ -5,46 +5,42 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        //JAVA BANKING PROGRAM FOR BEGINNERS
-
-        Scanner scanner = new Scanner(System.in);
         
-        //DECLARING VARIABLES
-
-        double balance =0;
-        boolean isRunning = true;
-        int choice;
-
-        while (isRunning) {
-                  
-        System.out.println("*****************");
-        System.out.println(" BANKING PROGRAM ");
-        System.out.println("*****************");
-        System.out.println("1. Show Balance");
-        System.out.println("2. Deposit");
-        System.out.println("3. Withdraw");
-        System.out.println("4. Exit");
-        System.out.println("*****************");
-
-        System.out.println("Enter your choice(1-4): ");
-        choice = scanner.nextInt();
-
-        switch (choice) {
-            case (1) -> ShowBalance(balance);
-            case (2) -> Deposit();
-            case (3) -> Withdraw(balance);
-            case (4) -> isRunning = false;
-
-            default -> System.out.println("Invalid Choice");
+                Scanner scanner = new Scanner(System.in);
+            
+            double balance =0;
+            boolean isRunning = true;
+            int choice;
+            
+            while (isRunning) {
+                
+                System.out.println("*****************");
+                System.out.println(" BANKING PROGRAM ");
+                System.out.println("*****************");
+                System.out.println("1. Show Balance");
+                System.out.println("2. Deposit");
+                System.out.println("3. Withdraw");
+                System.out.println("4. Exit");
+                System.out.println("*****************");
+                
+                System.out.println("Enter your choice(1-4): ");
+                choice = scanner.nextInt();
+                
+                switch (choice) {
+                    case (1) -> ShowBalance(balance);
+                    case (2) -> Deposit(scanner,balance);
+                    case (3) -> Withdraw(scanner,balance);
+                    case (4) -> isRunning = false;
+                    
+                    default -> System.out.println("Invalid Choice");
+                }
+                
+                
+                
+            }
         }
-
-
-
-        }
-
-        scanner.close();
         
-    }
+    
     static void ShowBalance(double balance){
         System.out.println("******************************");
 
@@ -53,34 +49,30 @@ public class App {
 
 
 
-    static void Deposit(){
-
-        Scanner scan = new Scanner(System.in);
+    static void Deposit(Scanner scanner ,double balance){
         double AddMoney = 0;
         System.out.println("*******************************");
 
         System.out.println("Please Enter the Amount");
-        AddMoney = scan.nextDouble();
+        AddMoney = scanner.nextDouble();
+        balance+=AddMoney;
 
         System.out.printf("The Deposited amount is %f",AddMoney);
 
-        scan.close();
+        
     }
 
-    
-    static void Withdraw(double balance){
 
-        Scanner scan = new Scanner(System.in);
+    static void Withdraw(Scanner scanner ,double balance){
         double SubMoney = 0;
         System.out.println("*******************************");
 
         System.out.println("Please Enter the Amount");
-        SubMoney = scan.nextDouble();
-        balance = balance - SubMoney;
+        SubMoney = scanner.nextDouble();
+        balance -= SubMoney;
 
         System.out.printf("The Withdrawed amount is %f",SubMoney);
 
-        scan.close();
     }
 
 
