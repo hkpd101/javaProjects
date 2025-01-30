ec6f9a88687aa06e291b626a7984a67cb1da7fec
HKPD101 <hrithikgowda006@gmail.com>
2025-01-30 22:01:40 +0530

    cxm

--- BankingProgram/src/App.java
+++ BankingProgram/src/App.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import jdk.jshell.spi.ExecutionControl;
 
 
 
@@ -12,10 +11,12 @@ public class App {
         
         //DECLARING VARIABLES
 
-        double balance;
+        double balance =0;
         boolean isRunning = true;
         int choice;
 
+        while (isRunning) {
+                  
         System.out.println("*****************");
         System.out.println(" BANKING PROGRAM ");
         System.out.println("*****************");
@@ -29,35 +30,58 @@ public class App {
         choice = scanner.nextInt();
 
         switch (choice) {
-            case (1):
-                    ShowBalance();
-                
-                break;
-            case (2):
-                    Deposit();
-                
-                break;
-            case (3):
-                    withdraw();
-                
-                break;
-            case (4):
-                    
-                
-                break; 
+            case (1) -> ShowBalance(balance);
+            case (2) -> Deposit();
+            case (3) -> Withdraw(balance);
+            case (4) -> isRunning = false;
 
-            default:
-                System.out.println("Invalid Choice");
+            default -> System.out.println("Invalid Choice");
         }
 
 
 
-
-
-
-
+        }
 
         scanner.close();
         
     }
+    static void ShowBalance(double balance){
+        System.out.println("******************************");
+
+        System.out.printf("The current Balance is %.2f",balance);
+    }
+
+
+
+    static void Deposit(){
+
+        Scanner scan = new Scanner(System.in);
+        double AddMoney = 0;
+        System.out.println("*******************************");
+
+        System.out.println("Please Enter the Amount");
+        AddMoney = scan.nextDouble();
+
+        System.out.printf("The Deposited amount is %f",AddMoney);
+
+        scan.close();
+    }
+
+    
+    static void Withdraw(double balance){
+
+        Scanner scan = new Scanner(System.in);
+        double SubMoney = 0;
+        System.out.println("*******************************");
+
+        System.out.println("Please Enter the Amount");
+        SubMoney = scan.nextDouble();
+        balance = balance - SubMoney;
+
+        System.out.printf("The Withdrawed amount is %f",SubMoney);
+
+        scan.close();
+    }
+
+
 }
